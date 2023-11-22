32ed1931deb3ae619a868ed7f09fcd7d173ddfd3
Gabrielle Zuba <118292803+gabszuba@users.noreply.github.com>
2023-11-22 15:21:06 -0300

    Refactor to use abc

--- simplexsolver/simplex/simplex_algorithm.py
+++ simplexsolver/simplex/simplex_algorithm.py
@@ -1,16 +1,87 @@
 import numpy as np
 import math
+from abc import ABC, abstractmethod
 
-class SimplexPrimal:
+class SimplexBase(ABC):
    def __init__(self, problem_type, num_of_variables, c, A, b, constraint):
       self.problem_type = problem_type
       self.tableau = np.array([])
       self.num_of_variables = num_of_variables
-      self.all_iteracions = []
+      self.all_iterations = []
       self.c = c
       self.A = A
       self.b = b
       self.constraint = constraint
+
+   @abstractmethod
+   def solve(self):
+      pass
+   
+   def add_slack_variables(self, m, i):
+      slack_variable = np.zeros(m)
+      slack_variable[i] = 1 
+      self.A = np.column_stack((self.A, slack_variable)) 
+      self.c = np.append(self.c, 0)
+
+   @abstractmethod
+   def add_variables(self):
+      pass
+
+   @abstractmethod
+   def get_formated_initial_tableau(self): 
+      pass
+
+   @abstractmethod
+   def is_not_optimum(self):
+      pass
+
+   def get_pivot_value(self, i, j):
+      return self.tableau[i][j]
+   
+   @abstractmethod
+   def get_pivot_row(self, entering_column):
+      pass
+
+   @abstractmethod
+   def get_entering_column(self):
+      pass
+
+   def update_tableau(self, pivot_row, entering_column, pivot_value):
+      new_tableau = np.zeros_like(self.tableau)
+
+      new_tableau[pivot_row] = np.round(self.tableau[pivot_row] / pivot_value, decimals=5)
+
+      for index, row in enumerate(self.tableau):
+         if pivot_row != index:
+            multiplier = -row[entering_column]
+            new_tableau[index] = np.round(multiplier * new_tableau[pivot_row] + self.tableau[index], decimals=5)
+      
+      self.all_iteracions.append(new_tableau.tolist())
+      self.tableau = new_tableau
+
+   def get_solution(self):
+      transposed = np.transpose(self.tableau)
+      solution = []
+      
+      for _, row in enumerate(transposed):
+         count_1 = np.count_nonzero(row == 1)
+         count_0 = np.count_nonzero(row == 0)
+
+         if count_1 == 1 and count_0 == len(row) - 1: 
+            index = np.argwhere(row==1)[0][0]
+            op_value = np.round(self.tableau[index][-1], decimals=2)
+            solution.append(op_value) 
+         else:
+            solution.append(0)
+
+      solution = (solution[:self.num_of_variables])
+      return solution
+
+
+
+class SimplexPrimal(SimplexBase):
+   def __init__(self, problem_type, num_of_variables, c, A, b, constraint):
+      super().__init__(problem_type, num_of_variables, c, A, b, constraint)
       self.M = 100
 
 
@@ -25,13 +96,6 @@ class SimplexPrimal:
          self.update_tableau(pivot_row, entering_column, pivot_value)
       
       return self.get_solution(), self.all_iteracions
-
-
-   def add_surplus_variables(self, m, i):
-      surplus_variable = np.zeros(m)
-      surplus_variable[i] = -1  
-      self.A = np.column_stack((self.A, surplus_variable))  
-      self.c = np.append(self.c, 0)
 
 
    def add_slack_variables(self, m, i):
@@ -91,10 +155,6 @@ class SimplexPrimal:
       return self.tableau[0]
 
 
-   def get_pivot_value(self, i, j):
-      return self.tableau[i][j]
-   
-
    def get_entering_column(self):
       objective_row = self.objective_row[:-1] #Array Numpy
       return np.argmin(objective_row)
@@ -103,7 +163,7 @@ class SimplexPrimal:
    def get_pivot_row(self, entering_column):
       pivot_column = (self.tableau[1:, entering_column])
       b_column = (self.tableau[1:, -1])
-      ratios = [] #Lista
+      ratios = [] 
       
       for i, element in enumerate(pivot_column):
          if element <= 0:
@@ -115,52 +175,10 @@ class SimplexPrimal:
       return pivot_row
 
 
-   def update_tableau(self, pivot_row, entering_column, pivot_value):
-      new_tableau = np.zeros_like(self.tableau)
-
-      new_tableau[pivot_row] = np.round(self.tableau[pivot_row] / pivot_value, decimals=5)
-
-      for index, row in enumerate(self.tableau):
-         if pivot_row != index:
-            multiplier = -row[entering_column]
-            new_tableau[index] = np.round(multiplier * new_tableau[pivot_row] + self.tableau[index], decimals=5)
-      
-      self.all_iteracions.append(new_tableau.tolist())
-      self.tableau = new_tableau
-
-
-   def get_solution(self):
-      transposed = np.transpose(self.tableau)
-      solution = []
-      
-      for _, row in enumerate(transposed):
-         count_1 = np.count_nonzero(row == 1)
-         count_0 = np.count_nonzero(row == 0)
-
-         if count_1 == 1 and count_0 == len(row) - 1: 
-            index = np.argwhere(row==1)[0][0]
-            op_value = np.round(self.tableau[index][-1], decimals=2)
-            solution.append(op_value) 
-         else:
-            solution.append(0)
-
-      solution = (solution[:self.num_of_variables])
-      return solution
-
-
-#Dualzinho <3
-class SimplexDual:
+class SimplexDual(SimplexBase):
 
    def __init__(self, problem_type, num_of_variables, c, A, b, constraint):
-      self.problem_type = problem_type
-      self.tableau = np.array([])
-      self.num_of_variables = num_of_variables
-      self.all_iteracions = []
-      self.c = c
-      self.A = A
-      self.b = b
-      self.constraint = constraint
-      self.M = 100
+      super().__init__(problem_type, num_of_variables, c, A, b, constraint)
 
    def solve(self):
       self.get_formated_intial_tableau()
@@ -173,13 +191,6 @@ class SimplexDual:
          self.update_tableau(pivot_row, entering_column, pivot_value)    
       return self.get_solution(), self.all_iteracions
    
-
-   def add_slack_variables(self, m, i):
-      slack_variable = np.zeros(m)
-      slack_variable[i] = 1 
-      self.A = np.column_stack((self.A, slack_variable)) 
-      self.c = np.append(self.c, 0)
-
    
    def add_variables(self):
       m, _ = self.A.shape
@@ -212,7 +223,6 @@ class SimplexDual:
 
 
    def get_entering_column(self, pivot_row):
-      """Tem que dar uma testada nisso aqui, parece meio estranho"""
       ratios = []
 
       for i, element in enumerate(self.tableau[pivot_row, :-1]):
@@ -225,39 +235,4 @@ class SimplexDual:
       return entering_column
 
 
-   def get_pivot_value(self, i, j):
-      return self.tableau[i][j]
 
-
-   def update_tableau(self, pivot_row, entering_column, pivot_value):
-      new_tableau = np.zeros_like(self.tableau)
-
-      new_tableau[pivot_row] = np.round(self.tableau[pivot_row] / pivot_value, decimals=5)
-
-      for index, row in enumerate(self.tableau):
-         if pivot_row != index:
-            multiplier = -row[entering_column]
-            new_tableau[index] = np.round(multiplier * new_tableau[pivot_row] + self.tableau[index], decimals=5)
-      
-      self.all_iteracions.append(new_tableau.tolist())
-      self.tableau = new_tableau
-
-
-   def get_solution(self):
-      transposed = np.transpose(self.tableau)
-      solution = []
-      
-      for _, row in enumerate(transposed):
-         count_1 = np.count_nonzero(row == 1)
-         count_0 = np.count_nonzero(row == 0)
-
-         if count_1 == 1 and count_0 == len(row) - 1: 
-            index = np.argwhere(row==1)[0][0]
-            op_value = np.round(self.tableau[index][-1], decimals=2)
-            solution.append(op_value) 
-         else:
-            solution.append(0)
-
-      solution = (solution[:self.num_of_variables])
-
-      return solution
